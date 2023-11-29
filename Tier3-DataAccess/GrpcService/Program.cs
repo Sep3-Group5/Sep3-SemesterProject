28f51e3dc0e8be412b120fb46ba91a25b1fede37
kubista9 <jakubkuka07@gmail.com>
2023-11-29 10:58:02 +0100

    removal from porgram.cs

--- Tier3-DataAccess/GrpcService/Program.cs
+++ Tier3-DataAccess/GrpcService/Program.cs
@@ -20,7 +20,6 @@ var app = builder.Build();
 app.MapGrpcService<PatientService>();
 app.MapGrpcService<DoctorService>();
 app.MapGrpcService<BookingService>();
-app.MapGrpcService<GreeterService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
