14d3c42e44bad31e37b2aad1a55d3833b42cc92b
kubista9 <jakubkuka07@gmail.com>
2023-12-04 16:51:04 +0100

    ViewAppointments initial commit

--- Tier1-Presentation/HttpClients/ClientInterfaces/IAppointmentService.cs
+++ Tier1-Presentation/HttpClients/ClientInterfaces/IAppointmentService.cs
@@ -6,4 +6,5 @@ namespace HttpClients.ClientInterfaces;
 public interface IAppointmentService
 {
     Task<Appointment> CreateAsync(AppointmentCreationDto dto);
+    Task<ICollection<Appointment>> GetAsync(int appointmentId, int patientId, int doctorId, string diagnostic, bool status, string date, string time);
 }
