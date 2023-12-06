0dfef422e31a9ea814141899dc45945c0276b78e
kubista9 <jakubkuka07@gmail.com>
2023-12-06 16:06:58 +0100

    bin files

--- Tier3-DataAccess/Domain/DTOs/PatientCreationDto.cs
+++ Tier3-DataAccess/Domain/DTOs/PatientCreationDto.cs
@@ -1,25 +1,6 @@
 ﻿namespace Domain.DTOs;
 
-public class PatientCreationDto
-{
-<<<<<<< Updated upstream
-    public int Id { get; }
-    public string Fullname { get; }
-    public string Username { get; }
-    public string Password { get; }
-
-    public PatientCreationDto(int id, string fullname, string username, string password)
-    {
-	    Id = id;
-	    Fullname = fullname;
-	    Username = username;
-	    Password = password;
-    }
-
-    public PatientCreationDto()
-    {
-    }
-=======
+public class PatientCreationDto {
 	public int Id { get; set; }
 	public string Fullname { get; set; }
 	public string Username { get; set; }
@@ -38,5 +19,4 @@ public class PatientCreationDto
 	{
 	}
 
->>>>>>> Stashed changes
 }
