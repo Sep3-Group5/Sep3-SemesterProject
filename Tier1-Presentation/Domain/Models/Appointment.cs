1542b5b12a10610169f50f327b8ae061d81242de
AlexGhitun <alexghitun@gmail.com>
2023-12-11 13:03:10 +0100

    Fixed showing appointment id for frontend

--- Tier1-Presentation/Domain/Models/Appointment.cs
+++ Tier1-Presentation/Domain/Models/Appointment.cs
@@ -2,10 +2,10 @@ namespace Domain.Models;
 
 public class Appointment
 {
-    public int AppointmentId { get; set; }
+    public int Id { get; set; }
     public int PatientId { get; set; }
     public int DoctorId { get; set; }
-    public string Diagnostic { get; set; }
+    public string Diagnosis { get; set; }
     public bool Status { get; set; }
     public string Date { get; set; }
     public string Time { get; set; }
